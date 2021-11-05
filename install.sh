f44915762d9a57621df59095da141a7c28e32ef5
triway340 <87687883+triway340@users.noreply.github.com>
2021-11-05 19:04:53 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo $YELLOW"[$RED!$YELLOW] Vamos a instalar los requerimientos para usar este script, escriba 's' si continua, de lo contrario escriba 'n' (sin las comillas)[s/n]"
 read inp
 if [ "$inp" = "s" ]; then
-   	apt update python3 && pkg install python3
+   	apt install python && pkg install python
    	pip install -r requirements.txt
     echo $GREEN"¡INSTALADO!"
 fi
